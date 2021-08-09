dd4a6d706c1e9d8e7a1d2b3bb09ef92251928edb
billw2012 <billw2011@gmail.com>
2021-08-09 22:38:17 +0100

    Make class level requirements mergable from defaults, with enable/disable and explicit level

--- BannerlordTwitch/BLTAdoptAHero/GlobalConfigs/GlobalHeroClassConfig.cs
+++ BannerlordTwitch/BLTAdoptAHero/GlobalConfigs/GlobalHeroClassConfig.cs
@@ -44,13 +44,19 @@ namespace BLTAdoptAHero
         public HeroClassDef FindClass(string search) 
             => ValidClasses.FirstOrDefault(c => c.Name.Equals(search, StringComparison.InvariantCultureIgnoreCase));
 
+        [Browsable(false), YamlIgnore]
+        public IEnumerable<ClassLevelRequirementsDef> ValidClassLevelRequirements 
+            => ClassLevelRequirements
+                .Where(c => c.Enabled && c.ClassLevel != 0)
+                .OrderBy(c => c.ClassLevel);
+        
         public int GetHeroClassLevel(Hero hero)
         {
             int level = 0;
-            foreach (var requirements in ClassLevelRequirements)
+            foreach (var requirements in ValidClassLevelRequirements)
             {
                 if (!requirements.IsMet(hero)) return level;
-                ++level;
+                level = requirements.ClassLevel;
             }
             return level;
         }
@@ -62,6 +68,11 @@ namespace BLTAdoptAHero
             SettingsHelpers.MergeCollections(
                 ClassDefs, 
                 Get(defaultSettings).ClassDefs,
+                (a, b) => a.ID == b.ID
+            );
+            SettingsHelpers.MergeCollections(
+                ClassLevelRequirements, 
+                Get(defaultSettings).ClassLevelRequirements,
                 (a, b) => a.ID == b.ID
             );
         }
@@ -88,49 +99,44 @@ namespace BLTAdoptAHero
         #endregion
     }
 
-    public class ClassLevelRequirementsDef : INotifyPropertyChanged, ILoaded, ICloneable
+    public class ClassLevelRequirementsDef : INotifyPropertyChanged, ICloneable
     {
-        [Description("Requirements for this class level"), PropertyOrder(1), UsedImplicitly, 
+        #region User Editable
+        [ReadOnly(true), UsedImplicitly]
+        public Guid ID { get; set; } = Guid.NewGuid();
+
+        [PropertyOrder(1), UsedImplicitly]
+        public bool Enabled { get; set; }
+                
+        [PropertyOrder(2), Description("Class level"), UsedImplicitly] 
+        public int ClassLevel { get; set; }
+
+        [Description("Requirements for this class level"), PropertyOrder(3), UsedImplicitly, 
          Editor(typeof(DerivedClassCollectionEditor<IAchievementRequirement>), 
              typeof(DerivedClassCollectionEditor<IAchievementRequirement>))]
         public ObservableCollection<IAchievementRequirement> Requirements { get; set; } = new();
-
-        [YamlIgnore, Browsable(false)]
-        public int ClassLevel => ClassConfig.ClassLevelRequirements.IndexOf(this) + 1;
+        #endregion
         
-        public ClassLevelRequirementsDef()
-        {
-            // For when these are created via the configure tool
-            ClassConfig = ConfigureContext.CurrentlyEditedSettings == null 
-                ? null : GlobalHeroClassConfig.Get(ConfigureContext.CurrentlyEditedSettings);
-        }
-        
+        #region Public Interface
         public override string ToString() 
-            => $"{ClassConfig.ClassLevelRequirements.IndexOf(this) + 1}: "
-                + (!Requirements.Any() 
-                ? "(none)" 
-                : string.Join(" + ", Requirements.Select(r => r.ToString())));
+            => $"{ClassLevel}: "
+               + (!Requirements.Any() 
+                   ? "(no requirements)" 
+                   : string.Join(" + ", Requirements.Select(r => r.ToString())));
 
+        public bool IsMet(Hero hero) => Requirements.All(r => r.IsMet(hero));
+        #endregion
+
+        #region ICloneable
         public object Clone() =>
             new ClassLevelRequirementsDef
             {
                 Requirements = new(CloneHelpers.CloneCollection(Requirements)),
             };
-
-        public bool IsMet(Hero hero) => Requirements.All(r => r.IsMet(hero));
-            
-        #region Implementation Details
-        [YamlIgnore, Browsable(false)]
-        private GlobalHeroClassConfig ClassConfig { get; set; }
         #endregion
                 
-        #region ILoaded
-        public void OnLoaded(Settings settings)
-        {
-            ClassConfig = GlobalHeroClassConfig.Get(settings);
-        }
-        #endregion
-        
+        #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
     }
 }
