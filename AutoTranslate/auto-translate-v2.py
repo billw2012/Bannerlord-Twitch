9bbefda5ee5b92ed234fb69c4154f8249ae2f26c
billw2012 <billw2011@gmail.com>
2021-09-04 13:11:53 +0100

    Add update/replace functionality to auto-translate, adding original attr to all translations

--- AutoTranslate/auto-translate-v2.py
+++ AutoTranslate/auto-translate-v2.py
@@ -14,32 +14,34 @@ def namespace(element):
     return m.group(1) if m else ''
 
 
-def translate(source_file, language, language_code, subdir, service_account_file, allow_replace):
+def translate(source_file, language, language_code, subdir, service_account_file, replace,
+              update_original, update_changed):
     translate_client = translate_v2.Client.from_service_account_json(service_account_file)
 
-    tree = etree.parse(source_file)
-    root = tree.getroot()
-    ns_map = {'': namespace(root)}
-    tags = root.find('tags', ns_map)
-    tag = tags.find('tag', ns_map)
-    tag.set('language', language)
+    source_tree = etree.parse(source_file)
+    source_root = source_tree.getroot()
+
+    ns_map = {'': namespace(source_root)}
 
     if not subdir:
         subdir = language_code.upper()
 
-    filebase, ext = os.path.splitext(os.path.basename(source_file))
-    target_file = os.path.join(os.path.dirname(source_file), subdir, f'{filebase}-{subdir}{ext}')
+    file_base, ext = os.path.splitext(os.path.basename(source_file))
+    target_file = os.path.join(os.path.dirname(source_file), subdir, f'{file_base}-{subdir}{ext}')
 
-    if not allow_replace and os.path.isfile(target_file):
+    if not (replace or update_original or update_changed) and os.path.isfile(target_file):
         print(f'Target file already exists, skipping {source_file}')
         return
 
-    def non_empty_tag(s):
-        tag = s.get('text')
-        return tag and tag.strip()
+    language_tag = source_root.find('tags', ns_map).find('tag', ns_map)
+    language_tag.set('language', language)
 
-    tags = list(filter(non_empty_tag, root.find('strings', ns_map).findall('string', ns_map)))
-    strings = [tag.get('text') for tag in tags]
+    def non_empty_tag(s):
+        text_attr = s.get('text')
+        return text_attr and text_attr.strip()
+
+    source_string_tags = list(filter(non_empty_tag, source_root.find('strings', ns_map).findall('string', ns_map)))
+    strings = [tag.get('text') for tag in source_string_tags]
 
     if len(strings) == 0:
         print(f'No translatable strings found, skipping {source_file}')
@@ -75,35 +77,64 @@ def translate(source_file, language, language_code, subdir, service_account_file
 
     translations = []
     CHUNK_SIZE = 10
+    INDENT = '  > '
+    print(INDENT, end='')
     for idx, c in enumerate(chunks(translatable_strings, CHUNK_SIZE)):
         print(f'{int(CHUNK_SIZE * idx * 100 / len(translatable_strings))}%..', end='')
         tchunk = translate_client.translate(c, source_language='en', target_language=language_code)
         ct = [t["translatedText"] for t in tchunk]
         translations.extend(ct)
     print(f'100%')
-    # try:
-    #     translations = translate_client.translate(translatable_strings, source_language='en', target_language=language_code)
-    #     # CHUNK_SIZE = 50
-    #     # for idx, c in enumerate(chunks(translatable_strings, CHUNK_SIZE)):
-    #     #     print(f'{int(CHUNK_SIZE * idx * 100 / len(translatable_strings))}%..', end='')
-    #     #     ct = translator.translate(c, src='en', dest=language_code)
-    #     #     if type(ct) == list:
-    #     #         translations.extend(ct)
-    #     #     elif ct:
-    #     #         translations.append(ct)
-    #     #     time.sleep(3)
-    #     # print(f'100%')
-    # except KeyboardInterrupt:
-    #     raise
-    # except Exception as e:
-    #     print('  Error occurred while translating, try again: ', e)
-    #     return
 
-    for tag, e, t in zip(tags, string_escapes, translations):
+    for tag, e, t, o in zip(source_string_tags, string_escapes, translations, strings):
         tag.set('text', unesc(t, e))
+        tag.set('original', o)
 
-    os.makedirs(os.path.dirname(target_file), exist_ok=True)
-    etree.ElementTree(root).write(target_file, encoding="utf-8", xml_declaration=True, pretty_print=True)
+    # If the file exists, and we didn't specify entirely replacing it, we will merge existing content, and warn on
+    # changed content
+    if not replace and os.path.isfile(target_file):
+        target_tree = etree.parse(target_file)
+        target_root = target_tree.getroot()
+        target_strings_root = target_root.find('strings', ns_map)
+        if target_strings_root is None:
+            # Just save the translated source directly and return
+            print(INDENT + f'Target file has no "strings" element, replacing entirely')
+            etree.ElementTree(source_root).write(target_file, encoding="utf-8", xml_declaration=True, pretty_print=True)
+            return
+        # Do merging by id
+        target_string_tags = list(filter(non_empty_tag, target_strings_root.findall('string', ns_map)))
+        for idx, source_string_tag in enumerate(source_string_tags):
+            source_id = source_string_tag.get('id')
+            source_original = source_string_tag.get('original')
+            source_text = source_string_tag.get('text')
+            try:
+                target_string_tag = next(t for t in target_string_tags if t.get('id') == source_id)
+                target_original = target_string_tag.get('original')
+                if target_original != source_original and (update_original or update_changed) or not target_original:
+                    target_string_tag.set('original', source_original)
+                    print(INDENT + f'Updated {source_id} original attribute')
+                    print(INDENT + f'  from "{target_original}"')
+                    print(INDENT + f'  to "{source_original}"')
+                target_text = target_string_tag.get('text')
+                if update_changed and target_text != source_text:
+                    target_string_tag.set('text', source_text)
+                    print(INDENT + f'Updated {source_id} text attribute')
+                    print(INDENT + f'  from "{target_text}"')
+                    print(INDENT + f'  to "{source_text}"')
+            except StopIteration:
+                target_string_tag = etree.SubElement(target_strings_root, 'string')
+                target_string_tag.set('id', source_id)
+                target_string_tag.set('text', source_text)
+                target_string_tag.set('original', source_original)
+                print(INDENT + f'Added {source_id}')
+                print(INDENT + f'  original "{source_original}"')
+                print(INDENT + f'  text "{source_text}"')
+        # Fix indents
+        etree.indent(target_root, space="    ")
+        etree.ElementTree(target_root).write(target_file, encoding="utf-8", xml_declaration=True, pretty_print=True)
+    else:
+        os.makedirs(os.path.dirname(target_file), exist_ok=True)
+        etree.ElementTree(source_root).write(target_file, encoding="utf-8", xml_declaration=True, pretty_print=True)
 
 
 if __name__ == "__main__":
@@ -111,13 +142,20 @@ if __name__ == "__main__":
     parser.add_argument('glob_patterns', metavar='glob', type=str, nargs='+',
                         help='globs (wild card patterns) describing what files to translate, '
                              'output file is determined automatically')
-    parser.add_argument('--account', dest='service_account_file', action='store', help='Path to Google Service Account Credentials json file', required=True)
-    parser.add_argument('--lang', dest='lang', action='store', help='Bannerlord language name',
-                        required=True)
-    parser.add_argument('--replace', dest='replace', action='store_true', help='Allow replacing of existing translation files (WARNING: overwrites any manual changes in the target file)')
-    parser.add_argument('--lang-code', dest='langcode', action='store', help='international language code for translation',
-                        required=True)
-    parser.add_argument('--subdir-override', dest='subdiroverride', action='store', help='Subdirectory override (usually it is uppercased lang-code)')
+    parser.add_argument('--account', dest='service_account_file', action='store',
+                        help='Path to Google Service Account Credentials json file', required=True)
+    parser.add_argument('--lang', dest='lang', action='store',
+                        help='Bannerlord language name', required=True)
+    parser.add_argument('--replace', dest='replace', action='store_true',
+                        help='Entirely replace target file. WARNING: overwrites any manual changes in the target file!')
+    parser.add_argument('--update-original-tag', dest='update_original_tag', action='store_true',
+                        help='Update the "original" tag in the target file, if it exists')
+    parser.add_argument('--update-changed', dest='update_changed', action='store_true',
+                        help='Update the translation if the original text has changed, implies update-original-tag')
+    parser.add_argument('--lang-code', dest='langcode', action='store',
+                        help='international language code for translation', required=True)
+    parser.add_argument('--subdir-override', dest='subdiroverride', action='store',
+                        help='Subdirectory override (usually it is uppercased lang-code)')
     args = parser.parse_args()
 
     expanded_files = [glob.glob(g, recursive=True) for g in args.glob_patterns]
@@ -127,4 +165,5 @@ if __name__ == "__main__":
         print('No files found matching the provided globs!')
     else:
         for f in unique_files:
-            translate(f, args.lang, args.langcode, args.subdiroverride, args.service_account_file, args.replace)
+            translate(f, args.lang, args.langcode, args.subdiroverride, args.service_account_file, args.replace,
+                      args.update_original_tag, args.update_changed)
